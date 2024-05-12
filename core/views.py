29c4f64eb3d5672fe70f74be2b9029f68fc9d035
abid <abidfazal398@gmail.com>
2024-05-12 10:01:33 +0500

    authentication features

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from item.models import Catagory,Item
+from .forms import SignUpForm
 
 # Create your views here.
 
@@ -14,5 +15,18 @@ def index(request):
 
 
 def contact(request):
-    return render(request,'core/contact.html')[0:6]
+    return render(request,'core/contact.html')
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else:
+        form = SignUpForm()
+    
+    return render(request,'core/signup.html',{'form':form})
 
--- puddle/urls.py
+++ puddle/urls.py
@@ -16,13 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from core.views import index,contact
 from django.conf import settings
 from django.conf.urls.static import static
+from core.views import contact
 
 urlpatterns = [
-    path('',index,name='index'),
+    path('',include('core.urls')),
     path('item/',include('item.urls')),
-    path('contact/',contact,name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
